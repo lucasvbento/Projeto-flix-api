b5d0ad97eed2e387a92fab91d153c6ddea44c776
lucas <lucasvbento1@gmail.com>
2025-05-22 21:20:05 -0300

    adicionando o app atores com o CRUD completo

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('genres/', GenreCreateListView.as_view(), name='genre'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-datail-view')
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-datail-view'),
+
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view')
 ]
