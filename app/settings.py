a38b281c01f66511099f6f3b1e5fb2d8023b24be
lucas <lucasvbento1@gmail.com>
2025-05-23 21:18:35 -0300

    adicionando o app de autenticaçao

--- app/settings.py
+++ app/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'genres',
     'actors',
     'movies',
     'reviews',
+    'authentication'
 
 ]
 
@@ -127,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
