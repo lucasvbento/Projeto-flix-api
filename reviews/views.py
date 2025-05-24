0af2c92bb79a6c252ff3d88000b1a62a648841f8
lucas <lucasvbento1@gmail.com>
2025-05-23 22:05:05 -0300

    Adicionando autenticaçao por token em todas as views

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,16 @@
 from reviews.models import Review
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from reviews.serializers import ReviewModelSerializer
 
 
 class ReviewCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
 
 
 class ReviewRetrieveUpdateDestroyViewModel(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
