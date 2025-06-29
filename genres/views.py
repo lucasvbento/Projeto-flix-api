639e05eef8c936da86d9ad0fdf8622b537de6c6d
lucas <lucasvbento1@gmail.com>
2025-06-29 15:03:38 -0300

    adicionando permissoes

--- genres/views.py
+++ genres/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from genres.serializers import GenreModelSerializer
+from app.permissions import GlobalDefaultPermission
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreModelSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreModelSerializer
--- reviews/views.py
+++ reviews/views.py
@@ -2,15 +2,16 @@ from reviews.models import Review
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from reviews.serializers import ReviewModelSerializer
+from app.permissions import GlobalDefaultPermission
 
 
 class ReviewCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
 
 
 class ReviewRetrieveUpdateDestroyViewModel(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
